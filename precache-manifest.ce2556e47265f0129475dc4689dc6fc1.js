3de0fe474a1224484bc9d0ae145c55e4193d349e
dsantos22 <dsantos@ibscards.com.au>
2020-01-06 21:18:33 +0800

    Updates

---
+++ precache-manifest.ce2556e47265f0129475dc4689dc6fc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d981b31f5502066ad95ae3cf848e470",
+    "url": "/full-stack-exam/index.html"
+  },
+  {
+    "revision": "2c3a99d17b1bbea7c818",
+    "url": "/full-stack-exam/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "11e924fe7a474755512d",
+    "url": "/full-stack-exam/static/css/main.89f12c3e.chunk.css"
+  },
+  {
+    "revision": "2c3a99d17b1bbea7c818",
+    "url": "/full-stack-exam/static/js/2.7659b5c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/full-stack-exam/static/js/2.7659b5c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11e924fe7a474755512d",
+    "url": "/full-stack-exam/static/js/main.6d1bb5e4.chunk.js"
+  },
+  {
+    "revision": "7cecf431bf809214705f",
+    "url": "/full-stack-exam/static/js/runtime-main.9f520bc2.js"
+  }
+]);
